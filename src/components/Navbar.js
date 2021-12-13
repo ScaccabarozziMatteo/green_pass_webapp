bfd8191301e43a98ead328192016a54d04a26c05
gccianmario <s262472@studenti.polito.it>
2021-12-13 23:37:26 +0100

    updated navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,33 +10,32 @@ import {NavLink} from "react-router-dom";
 function Navbar() {
 
     return (
-        <AppBar position="static">
+        <AppBar position='sticky'>
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <NavLink style={{textDecoration: "none"}} to='/'>
+                <Toolbar style={{display:"flex", justifyContent:"flex-start"}} disableGutters>
+                    <NavLink style={{textDecoration: "none", marginLeft:"2rem"}} to='/'>
                         <Button
-                            sx={{my: 1, color: 'white', display: 'flex', fontSize: '20px'}}
-                            size={"large"}
+                            sx={{my: 1, color: 'white', display: 'flex'}}
                         >
-                            Green Pass Checker
+                            Home
                         </Button>
                     </NavLink>
 
-                        <NavLink style={{textDecoration: "none"}} to='/checker'>
-                            <Button
-                                sx={{my: 1, color: 'white', display: 'flex'}}
-                            >
-                                Check
-                            </Button>
-                        </NavLink>
+                    <NavLink style={{textDecoration: "none", marginLeft:"2rem"}} to='/checker'>
+                        <Button
+                            sx={{my: 1, color: 'white', display: 'flex'}}
+                        >
+                            Check
+                        </Button>
+                    </NavLink>
 
-                        <NavLink style={{textDecoration: "none"}} to='/generator'>
-                            <Button
-                                sx={{my: 1, color: 'white', display: 'flex'}}
-                            >
-                                QR Generator
-                            </Button>
-                        </NavLink>
+                    <NavLink style={{textDecoration: "none", marginLeft:"2rem"}} to='/generator'>
+                        <Button
+                            sx={{my: 1, color: 'white', display: 'flex'}}
+                        >
+                            QR Generator
+                        </Button>
+                    </NavLink>
 
                 </Toolbar>
             </Container>
