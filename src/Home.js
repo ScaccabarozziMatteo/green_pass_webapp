a92db2633c0735a5a5dd9195498a7f933fd49ed7
gccianmario <s262472@studenti.polito.it>
2021-12-13 23:37:42 +0100

    updated homepage description

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,12 @@ import {Typography} from "@mui/material";
 
 export default function Home() {
     return(
-        <div>
-            <Typography variant={"h2"}>Welcome on our webapp!</Typography>
-            <Typography variant={"h6"}>You can check a Green Pass validity or you can generate a Green Pass QR code.</Typography>
+        <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
+            <Typography style={{marginTop:"1rem"}} variant={"h2"}>Welcome on our webapp!</Typography>
+            <Typography style={{marginTop:"1rem"}} variant={"h6"}>You can check a Green Pass validity or you can generate a Green Pass QR code.</Typography>
             <Typography variant={"h6"}>Enjoy :)</Typography>
+            <Typography style={{marginTop:"1rem"}} variant={"h6"}>The id contained in the QR code is checked on our MongoDb</Typography>
+            <Typography style={{marginTop:"1rem"}} variant={"h6"}>The personal data encoded in the QR code is shown togheter with the validity </Typography>
         </div>
     )
 }
