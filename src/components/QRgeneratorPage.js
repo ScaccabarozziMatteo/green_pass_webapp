193fe4b2a28bf73ca0bb4e4c465c6de3b7d98586
gccianmario <s262472@studenti.polito.it>
2021-12-13 03:26:42 +0100

    added the query machinery
    DB source to change

--- src/components/QRgeneratorPage.js
+++ src/components/QRgeneratorPage.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import QRCode from "react-qr-code";
-import {Typography} from "@mui/material";
-// Import the MongoDB Realm Web SDK
+import {Button, Typography} from "@mui/material";
 import * as Realm from "realm-web";
 
 //component need to convert string in bson
@@ -14,38 +13,17 @@ const mongodb = app.currentUser.mongoClient("mongodb-atlas");
 //const people = mongodb.db("covid_19_management").collection("people");
 const people = mongodb.db("new_york_data").collection("new_york_crashes");
 
-/* DEKETE IN FINAL VERSION
-const PersonSchema = {
-    name: "Person",
-    properties: { //person
-        _id: "objectId",
-        name: "string",
-        surname: "string",
-        bday: "date",
-        address: "string",
-        zip_code: "string",
-        city: "string",
-        email: "string",
-        telephone_number: "int",
-        vaccines: "data",
-        tests: "data",
-        emergency_contact: "data?"
-    }
-    ,
-    primaryKey: "_id",
-};
-*/
 
-// Create a component that displays the given user's details
+// component to display DB user data
 function UserDetail({ user }) {
     return (
         <div>
-            <h1>Logged in with id: {user.id}</h1>
+            <h1>Logged in with user id: {user.id}</h1>
         </div>
     );
 }
 
-// Create a component that lets an anonymous user log in
+// component for login in the realm app
 const  Login = ({setUser}) => {
     const loginAnonymous = async () => {
         //const user = await app.logIn(Realm.Credentials.emailPassword('test@gmail.com', 'test123'));
@@ -53,22 +31,34 @@ const  Login = ({setUser}) => {
         console.log(user)
         setUser(user)
     };
-    return <button onClick={loginAnonymous}>Log In</button>;
+    return <Button
+        variant={"contained"}
+        style={{background: 'green', margin: '10% 35%'}}
+        onClick={loginAnonymous}
+    >
+        Log In
+    </Button>;
 }
+
+// wrapper component that run the query
 const  RunQuery = ({fetchFunction,setPerson, searchId}) => {
-    return <button onClick={()=>fetchFunction(setPerson,searchId)}>Run Query</button>;
+    return <Button
+        variant={"contained"}
+        style={{background: 'green', margin: '10% 35%'}}
+        onClick={()=>fetchFunction(setPerson,searchId)}
+    > Generate QR</Button>;
 }
 
+// use this function setting the searchID to get a person with that id
 const fetchPersonById = async (setPersonObject, searchId) =>{
-
-    //setPersonObject(person)
-    //console.log(person)
-    //const numPlants = await people.count();
-    people.findOne({ "_id" : ObjectID(searchId)}).then((p)=>console.log(p)) //need to convert in BSON
-    //console.log(`There are ${numPlants} plants in the collection`);
+    people.findOne({ "_id" : ObjectID(searchId)})
+        .then((p)=>{
+            setPersonObject(p)
+            console.log(p)
+        })
 }
 
-export default function QRgeneratorPage() {
+const QRgeneratorPage = () => {
     const [user, setUser] = useState(app.currentUser);
     const [person, setPerson] = useState(null);
 
@@ -77,20 +67,16 @@ export default function QRgeneratorPage() {
         <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
 
             <Typography variant="h2" align='center'>Green Pass QR generator</Typography>
-
-            <Login />
-
-            <QRCode
-                level="H"
-                value={JSON.stringify({
-                    name: 'Matteo',
-                    surname: 'Scaccabarozzi',
-                    birthdate: '02-04-1996',
-                    validity: true
-                })}
-            />
+            {person ?
+                <QRCode
+                    level="H"
+                    value={JSON.stringify(person)}
+                />:null
+            }
             <RunQuery fetchFunction={fetchPersonById} setPerson={setPerson} searchId={"617fcaf8db758795fcadb1a7"}/>
             {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
         </div>
     )
 }
+
+export default QRgeneratorPage
